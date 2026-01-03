182178a0412b750fd6a132c10407bab4ca6ae167
siddh-07 <siddhbhadani26@gmail.com>
2026-01-02 22:06:53 -0600

    Use of OS Module

--- Chapter1/problem3.py
+++ Chapter1/problem3.py
@@ -1,3 +1,10 @@
 import os
 
-directory_path = 'Chapter1'
+directory_path = '/Users/siddhbhadani/Documents/GitHub'
+
+# Get the list of files and directories in the specified path
+contets = os.listdir(directory_path)
+
+# Print the contents of the directory
+for item in contets:
+    print(item)
