5d8f67ab9eca9c36a3ac7eccc94bf318cfbcc4e4
siddh-07 <siddhbhadani26@gmail.com>
2026-01-03 00:30:42 -0600

    add practice sets of chapter 2

---
+++ Chapter2/question1.py
@@ -0,0 +1,27 @@
+# Write a python program to add two numbers.
+
+number1 = 1
+number2 = 2.5
+
+sum_result = number1 + number2
+
+print("The sum of", number1, "and", number2, "is:", sum_result)
+
+# Write a program to find remainder when a number is divided by 2.
+number = 19
+remainder = number % 5
+print("The remainder when", number, "is divided by 5 is:", remainder)
+
+#Write a program to check type of variable.
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello, " + name + "! You are " + age + " years old.")
+print("Type of name variable:", type(name))
+print("Type of age variable:", type(age))
+
+# Write a program to check whether a number is greater than another number or not.
+a = 34
+b = 80 
+
+print("Is b is greater than a or not?:", b > a)
---
+++ Chapter2/question2.py
@@ -0,0 +1,8 @@
+# This program calculates the average of two integers provided by the user.
+
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+
+print("\The avarage of", a, "and", b, "is:", (a + b) / 2)
+
+print("The square of first integer is:", a ** 2)
