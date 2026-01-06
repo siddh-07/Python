28a7eac77ba2ecf1bf33f0ef5f3614c12f38e3ec
siddh-07 <siddhbhadani26@gmail.com>
2026-01-05 20:54:15 -0600

    Create Problems.py

---
+++ Chapter4/Problems.py
@@ -0,0 +1,21 @@
+# Practise Code for Chapter 4 
+# Problem 1: Write a program to create a list of fruits by taking input from the user.
+Fruits = []
+f1 = input("Enter the first element of the list: ")    
+f2 = input("Enter the second element of the list: ")
+f3 = input("Enter the third element of the list: ")
+f4 = input("Enter the fourth element of the list: ")
+f5 = input("Enter the fifth element of the list: ")
+f6 = input("Enter the sixth element of the list: ")
+f7 = input("Enter the seventh element of the list: ")
+Fruits.append(f1)
+Fruits.append(f2)
+Fruits.append(f3)
+Fruits.append(f4)
+Fruits.append(f5)
+Fruits.append(f6)
+Fruits.append(f7)
+
+print(Fruits)   
+
+
