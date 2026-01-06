5be1bf0326c383c300d680bf4ca8c76b94180050
siddh-07 <siddhbhadani26@gmail.com>
2026-01-05 20:16:22 -0600

    lists Methods

---
+++ Chapter4/list.py
@@ -0,0 +1,10 @@
+# Basic example of a list in Python
+
+friends = ["Siddh","Harry",1,True,5.6,"Ron"]
+
+print(friends[0])
+
+friends[0] = "Draco" # Unlike strings, lists are mutable (changeable)
+print(friends[0])
+
+print(friends[1:4]) # Slicing a list
---
+++ Chapter4/list_methods.py
@@ -0,0 +1,7 @@
+friends = ["Siddh","Harry",1,True,5.6,"Ron"]
+# Before using list methods
+print(friends)
+
+# Unlike strings list modification methods do not return a new list, they modify the existing list and return None.
+print(friends.append("Hermione"))  # Adds "Hermione" to the end of the list
+
