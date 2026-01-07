ba1059802d86829fbea1b860fc6de8d7bf63c215
siddh-07 <siddhbhadani26@gmail.com>
2026-01-06 21:04:15 -0600

    Update dictionary.py

--- Chapter5/dictionary.py
+++ Chapter5/dictionary.py
@@ -1,6 +1,8 @@
  # Example of using dictionaries in Python
  # Value at left side is the key and value at right side is the value associated with that key
  # Its is mutable i.e we can change, add or remove key-value pairs after its creation
+ # Dictionaries are unordered collections of key-value pairs
+ # In the dictionary, keys must be unique and immutable (like strings, numbers, or tuples), while values can be of any data type and can be duplicated.
  
 marks = {
     "Alice": 85,
