0faccf5954e282b4f06814aa21066d719bb93d45
siddh-07 <siddhbhadani26@gmail.com>
2026-01-07 14:53:21 -0600

    Create whileLoop.py

---
+++ Chapter7/whileLoop.py
@@ -0,0 +1,14 @@
+ # Write a program that uses a while loop to print the numbers from 1 to 50.
+ 
+num = 1
+ 
+while (num <= 50):
+    print(num)
+    num += 1    
+    
+l1 = [1, 2, 3, 4, 5,"Siddh","John",False,9.8]
+
+i = 0
+while (i < len(l1)):
+    print(l1[i])
+    i += 1  
