f5d00f5f3f18ecf78182663e4bdd25c190f34524
siddh-07 <siddhbhadani26@gmail.com>
2026-01-06 20:10:01 -0600

    Create dictionary.py

---
+++ Chapter5/dictionary.py
@@ -0,0 +1,12 @@
+ # Example of using dictionaries in Python
+ # Value at left side is the key and value at right side is the value associated with that key
+ # Its is mutable i.e we can change, add or remove key-value pairs after its creation
+ 
+marks = {
+    "Alice": 85,
+    "Bob": 92,
+    "Charlie": 78,
+    "Siddh" : 99,
+    }
+
+print("Marks Dictionary:", marks)
