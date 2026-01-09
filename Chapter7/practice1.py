37108491295a2df781f3d834c5c52c2f1f75f568
siddh-07 <siddhbhadani26@gmail.com>
2026-01-08 20:26:36 -0600

    Create practice1.py

---
+++ Chapter7/practice1.py
@@ -0,0 +1,31 @@
+# This program prints the multiplication table of a given positive integer.
+# num = int(input("Enter a positive integer: "))
+
+# Using for loop
+# for i in range(1,11):
+#     print(f"{num} x {i} = {num * i}")
+
+# Using while loop
+# i = 1        
+# while (i < 11):
+#     print(f"{num} x {i} = {num * i}")
+#     i += 1
+    
+# Write a program that iterates through a list of names and prints a greeting for each name that starts with the letter 'S'.
+# l = ["Siddh","Nency","Sam","John","Doe"]
+
+# for name in l:
+#     if name.startswith("S"):
+#         print(f"Hello, {name}!")
+  
+#Write a program that checks if a given number is prime or not using a for loop with an else clause.
+# num = int(input("Enter a positive integer: "))
+
+# for i in range(2,num):
+#     if (num % i) == 0:
+#         print(f"{num} is not a prime number.")
+#         break
+# else:  # This else corresponds to the for loop
+#     print(f"{num} is a prime number.")
+    
+ 
