467a3a30e88e188522eff38c887db48410ff7969
siddh-07 <siddhbhadani26@gmail.com>
2026-01-10 20:31:17 -0600

    Update practice3.py

--- Chapter7/practice3.py
+++ Chapter7/practice3.py
@@ -2,13 +2,14 @@
 
 num = int(input("Enter the number of rows: "))
 
-# Print right-angled triangle pattern
+print("Right-angled triangle pattern:")
 for i in range(num):
     for j in range(i+1):
-        print("*", end=" ")
+        print(f"*", end=" ")
     print() 
 
-# Print hollow square pattern
+
+print("\nHollow square pattern:")
 for i in range(num):
     for j in range(num):
         if j == 0 or j == num - 1 or i == 0 or i == num - 1:
@@ -16,5 +17,15 @@ for i in range(num):
         else:
             print(" ",end=" ")
     print()  # Move to the next line after inner loop    
+    
+print("\nSolid square pattern:")
+for i in range(num):
+    for j in range(num):
+        print("*", end=" ")
+    print()  # Move to the next line after inner loop
 
-   
+print("\nTringle Pattern:")
+for i in range(1,num+1):
+    print(" " * (num-i),end="")
+    print("*"* (2*i-1), end="")
+    print("\n")   
