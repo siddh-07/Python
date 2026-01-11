c7f37a82996c71aec5ec7df8e025a3b11c5317c7
siddh-07 <siddhbhadani26@gmail.com>
2026-01-10 21:47:12 -0600

    Update practiceOfPattern.py

--- Chapter7/practiceOfPattern.py
+++ Chapter7/practiceOfPattern.py
@@ -29,10 +29,20 @@ for i in range(num+1,1,-1):
         print(j,end="") 
     print()
     
-print("Floyd’s Triangle:")
+print("\nFloyd’s Triangle:")
 n = 1   
 for i in range(1,num+1):
     for j in range(1,i+1):
         print(f"{n} ",end="")
         n += 1
     print()  
+    
+print("\n\"X\" Pattern:")
+
+for i in range(1,num+1,):
+    for j in range(1,num+1):
+        if (i == j ):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print("")    
