8ed0d104911044abe7a64929862e570aaeeb8cb4
siddh-07 <siddhbhadani26@gmail.com>
2026-01-10 21:01:15 -0600

    Create practiceOfPattern.py

---
+++ Chapter7/practiceOfPattern.py
@@ -0,0 +1,30 @@
+# Example of Patterns
+
+num = 5
+
+# Inverted Trinagle Pattern
+print("Inverted Trinagle Pattern:")
+for i in range(num, 0,-1):
+    print("*" * i)
+    
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+print("\nPrint numbers in triangle:")
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print(j,end="") 
+    print()       
+    
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1    
+print("\nReverse number triangle:")
+for i in range(num+1,1,-1):
+    for j in range(1,i):
+        print(j,end="") 
+    print()    
