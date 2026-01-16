000036763767060eed8422d96b12258d6431dbda
siddh-07 <siddhbhadani26@gmail.com>
2026-01-16 11:18:21 -0600

    Create main.py

---
+++ Project1/main.py
@@ -0,0 +1,12 @@
+# Create a Stone PaperScissors game
+import random
+computer = random.choice(['stone', 'paper', 'scissors'])
+user = input("Enter your choice (stone, paper, scissors): ").lower()
+
+if user == computer:
+    print(f"Both chose {user}. It's a tie!")
+elif (user == 'stone' and computer == 'scissors') or (user == 'paper' and computer == 'stone') or (user == 'scissors' and computer == 'paper'):
+        print(f"You chose {user} and the computer chose {computer}. You win!")
+else:
+    print(f"You chose {user} and the computer chose {computer}. You lose!")
+        
