07d7ee9fd3d8c9f49c2e5a1e9a15b13be8687429
siddh-07 <siddhbhadani26@gmail.com>
2026-01-16 01:03:09 -0600

    Create fuctions.py

---
+++ Chapter8/fuctions.py
@@ -0,0 +1,12 @@
+# Function Defination
+
+def avg():
+    a = int(input("Enter Number: "))
+    b = int(input("Enter Number: "))
+    c = int(input("Enter Number: "))
+    
+    print(f"Avarage of given numbers is {(a+b+c)/3}")
+    
+# Function Call
+avg()
+    
