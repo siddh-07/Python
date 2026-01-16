a642aca03a20a10f8561ef1c0b5901ddbf73a288
siddh-07 <siddhbhadani26@gmail.com>
2026-01-16 11:04:44 -0600

    Update practice2.py

--- Chapter8/practice2.py
+++ Chapter8/practice2.py
@@ -10,3 +10,18 @@ pattern(5)
 # Write a Python function to convert inches to centimeters
 def inches_to_cm(inches):
     return inches * 2.54
+
+inch = int(input("Enter inches: "))
+print(f"{inch} inches is equal to {inches_to_cm(inch)} centimeters.")
+
+# Remove word and strip from a list
+def removeWord(list,word): 
+    n = []
+    for i in list:
+        if not(word == i):
+            n.append(i.strip(word))
+    return n
+
+l = ["  apple  "," banana ","  cherry "," date " , " na "]
+
+print(removeWord(l," "))
