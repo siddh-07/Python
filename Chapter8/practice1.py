c673706e6fc628de37915f4fd1b6d53d2cb1398d
siddh-07 <siddhbhadani26@gmail.com>
2026-01-16 10:49:02 -0600

    Update practice1.py

--- Chapter8/practice1.py
+++ Chapter8/practice1.py
@@ -15,3 +15,26 @@ num3 = float(input("Enter third number: "))
 greatest_number = greatest(num1, num2, num3)
 print(f"The greatest number among {num1}, {num2}, and {num3} is {greatest_number}")
 
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+# Input from user
+celsius_temp = float(input("\nEnter temperature in Celsius: "))
+fahrenheit_temp = celsius_to_fahrenheit(celsius_temp)
+print(f"{celsius_temp}°C is equal to {fahrenheit_temp}°F")
+
+def sumOfNaturalNumbers(n):
+    """Return the sum of first n natural numbers."""
+    if n == 0:
+        return 0
+    elif n < 1:
+        return "Invalid Input"
+    else:
+        return n + sumOfNaturalNumbers(n - 1)
+    
+# Input from user
+num = int(input("\nEnter a positive integer: "))
+sum_natural = sumOfNaturalNumbers(num)
+print(f"The sum of the first {num} natural numbers is {sum_natural}")
