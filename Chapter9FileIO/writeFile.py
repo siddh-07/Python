39005ec50dbf80f33540d8a824c77454d286e80d
siddh-07 <siddhbhadani26@gmail.com>
2026-01-16 11:46:05 -0600

    Create writeFile.py

---
+++ Chapter9FileIO/writeFile.py
@@ -0,0 +1,8 @@
+with open("Chapter9FileIO/file.txt", "w") as file:
+    file.write("Hello, World!\n")
+    file.write("This is a test file.\n")
+    file.write("Writing to files in Python is easy!\n")
+    file.write("My Name is Siddh Bhadani\n")
+    
+    
+    
