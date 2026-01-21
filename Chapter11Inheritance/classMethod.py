8e670cfee492d482f50ea2e10812b19fc71b7129
siddh-07 <siddhbhadani26@gmail.com>
2026-01-21 10:19:45 -0600

    Update classMethod.py

--- Chapter11Inheritance/classMethod.py
+++ Chapter11Inheritance/classMethod.py
@@ -1,12 +1,24 @@
 class Employee:
     
-    name = "Siddh"
+    company_name = "Google"
     
     @classmethod
     def show(cls):
-        print(f"The class attribute of name is {cls.name}.")
+        print(f"The class attribute of name is {cls.company_name}.")
+        
+    @property
+    def display(self):
+        print(f"The first name is {self.fname} and last name is {self.lname}.")
+        
+    @display.setter
+    def name(self, value):
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
         
         
 emp = Employee()
-emp.name = "John" # Instance attribute
-emp.show()  # Output: The class attribute of name is Siddh. because class method uses class attribute not instance attribute
+emp.company_name = "Microsoft" # Instance attribute
+emp.show()  # Output: The class attribute of name is Google. because class method uses class attribute not instance attribute
+
+emp.name = "Siddh Bhadani"
+emp.display  # Output: The first name is Siddh and last name is Bhadani. because property uses instance attribute
