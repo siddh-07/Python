f3bc233afe703103851ea3ad4f5e267d57124180
siddh-07 <siddhbhadani26@gmail.com>
2026-01-21 09:29:16 -0600

    Create problem2.py

---
+++ Chapter10OOPS/problem2.py
@@ -0,0 +1,24 @@
+class Calculator: 
+    
+    def __init__(self, number):
+        self.number = number
+        
+    def squre(self):
+        return self.number ** 2
+    
+    def cube(self):
+        return self.number ** 3
+    
+    def square_root(self):
+        return self.number ** 0.5
+    
+calc = Calculator(9)
+print("Square:", calc.squre())
+print("Cube:", calc.cube())
+print("Square Root:", calc.square_root())
+
+# Output:
+# Square: 81
+# Cube: 729
+# Square Root: 3.0
+        
